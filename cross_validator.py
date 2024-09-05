3f411c43b93b1497382a48d013f68a8f67ce5b26
Nikolar1 <nikolajazak@gmail.com>
2024-09-05 10:34:00 +0200

    Optimized classifiers and cross validation

--- cross_validator.py
+++ cross_validator.py
@@ -4,6 +4,8 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from collections import Counter
+
+from datasets import datasets
 from utils import calculate_bad_hubness_weights, calculate_lid
 import matplotlib.pyplot as plt
 
@@ -11,10 +13,11 @@ import dataset_analysis
 
 
 class knn_classifier:
+    _name = "knn_classifier"
     def __init__(self, k=3):
         self.k = k
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, pre_trained_classifier_lid = None, pre_trained_classifier_hubness = None):
         self.X_train = X_train
         self.y_train = y_train
 
@@ -32,14 +35,18 @@ class knn_classifier:
         most_common = Counter(k_nearest_labels).most_common(1)
         return most_common[0][0]
 
+    def get_name(self):
+        "knn_classifier"
+
 class knn_classifier_hubness_weighted:
+    _name = "knn_classifier_hubness_weighted"
     def __init__(self, k=3):
         self.k = k
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, pre_trained_classifier_lid = None, pre_trained_classifier_hubness = None):
         self.X_train = X_train
         self.y_train = y_train
-        self.weights = calculate_bad_hubness_weights(X_train, y_train, n_jobs=12, n_neighbors=self.k)
+        self.weights = calculate_bad_hubness_weights(X_train, y_train, n_jobs=12, n_neighbors=self.k, pre_trained_classifier=pre_trained_classifier_hubness)
 
     def predict(self, X_test):
         predictions = [self._predict(x) for x in X_test]
@@ -60,14 +67,18 @@ class knn_classifier_hubness_weighted:
         most_common = weighted_votes.most_common(1)
         return most_common[0][0]
 
+    def get_name(self):
+        "knn_classifier_hubness_weighted"
+
 class knn_classifier_lid:
+    _name = "knn_classifier_lid"
     def __init__(self, k=3):
         self.k = k
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, pre_trained_classifier_lid = None, pre_trained_classifier_hubness = None):
         self.X_train = X_train
         self.y_train = y_train
-        self.weights = calculate_lid(X_train, y_train, n_jobs=12, n_neighbors=self.k)
+        self.weights = calculate_lid(X_train, y_train, n_jobs=12, n_neighbors=100, pre_trained_classifier=pre_trained_classifier_lid)
 
     def predict(self, X_test):
         predictions = [self._predict(x) for x in X_test]
@@ -89,13 +100,14 @@ class knn_classifier_lid:
         return most_common[0][0]
 
 class knn_classifier_lid_and_hubness_weight:
+    _name = "knn_classifier_lid_and_hubness_weight"
     def __init__(self, k=3):
         self.k = k
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, pre_trained_classifier_lid = None, pre_trained_classifier_hubness = None):
         self.X_train = X_train
         self.y_train = y_train
-        self.weights = calculate_lid(X_train, y_train, n_jobs=12, n_neighbors=self.k) + calculate_bad_hubness_weights(X_train, y_train, n_jobs=12, n_neighbors=self.k)
+        self.weights = calculate_lid(X_train, y_train, n_jobs=12, n_neighbors=100, pre_trained_classifier=pre_trained_classifier_lid) + calculate_bad_hubness_weights(X_train, y_train, n_jobs=12, n_neighbors=self.k, pre_trained_classifier=pre_trained_classifier_hubness)
 
     def predict(self, X_test):
         predictions = [self._predict(x) for x in X_test]
@@ -116,29 +128,46 @@ class knn_classifier_lid_and_hubness_weight:
         most_common = weighted_votes.most_common(1)
         return most_common[0][0]
 
+    def get_name(self):
+        "knn_classifier_lid_and_hubness_weight"
 
-def cross_validate(X, y, classifier):
+
+def cross_validate(X, y, classifiers, n_jobs=1, n_neighbours=3):
     X = np.array(X)
     y = np.array(y)
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
+    knn_hubness = KNeighborsClassifier(n_neighbors=n_neighbours, n_jobs=n_jobs)
+    knn_lid = KNeighborsClassifier(n_neighbors=100, n_jobs=n_jobs)
 
-    accuracies = []
-
+    accuracies = {}
+    for classifier in classifiers:
+        accuracies.update({classifier._name: []})
     for train_index, test_index in kf.split(X):
         x_train, x_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
-        classifier.fit(x_train, y_train)
+        knn_lid.fit(x_train, y_train)
+        knn_hubness.fit(x_train, y_train)
 
-        y_pred = classifier.predict(x_test)
-        accuracy = accuracy_score(y_test, y_pred)
+        for classifier in classifiers:
+            classifier_accuracies = accuracies.get(classifier._name)
+            classifier.fit(x_train, y_train, pre_trained_classifier_lid=knn_lid, pre_trained_classifier_hubness=knn_hubness)
 
-        accuracies.append(accuracy)
+            y_pred = classifier.predict(x_test)
+            accuracy = accuracy_score(y_test, y_pred)
 
-    return np.average(accuracies), np.mean(accuracies), accuracies
+            classifier_accuracies.append(accuracy)
+            accuracies.update({classifier._name: classifier_accuracies})
+    for classifier in classifiers:
+        average = np.average(accuracies.get(classifier._name))
+        mean = np.mean(accuracies.get(classifier._name))
+        accuracy = accuracies.get(classifier._name)
+        accuracies.update({classifier._name: (average, mean, accuracy)})
+    return accuracies
 
 if __name__ == '__main__':
-    for dataset in ["vehicle"]:
+    datasets = ["mfeat-factors", "mfeat-fourier", "optdigits", "segment", "spectrometer", "vehicle"]
+    for dataset in datasets:
         Y, X = dataset_analysis.load_dataset(dataset)
         # scaler = StandardScaler()
         # X = scaler.fit_transform(X)
@@ -148,15 +177,23 @@ if __name__ == '__main__':
         knn_lid_weighted_scores = []
         knn_lid_and_hubness_weighted_scores = []
 
+
         for k in k_values:
-            knn_score = cross_validate(np.array(X), np.array(Y), knn_classifier(k=k))[1]
-            knn_hubness_weighted_score = cross_validate(np.array(X), np.array(Y), knn_classifier_hubness_weighted(k=k))[1]
+            classifiers = [
+                knn_classifier(k=k),
+                knn_classifier_hubness_weighted(k=k),
+                # knn_classifier_lid(k=k),
+                # knn_classifier_lid_and_hubness_weight(k=k)
+            ]
+            accuracy_scores = cross_validate(np.array(X), np.array(Y), classifiers)
+            knn_score = accuracy_scores.get(classifiers[0]._name)[1]
+            knn_hubness_weighted_score = accuracy_scores.get(classifiers[1]._name)[1]
 
             knn_scores.append(knn_score)
             knn_hubness_weighted_scores.append(knn_hubness_weighted_score)
 
-            # knn_lid_weighted_score = cross_validate(np.array(X), np.array(Y), knn_classifier_lid(k=k))[1]
-            # knn_lid_and_hubness_weighted_score = cross_validate(np.array(X), np.array(Y), knn_classifier_lid_and_hubness_weight(k=k))[1]
+            # knn_lid_weighted_score = accuracy_scores.get(classifiers[2]._name)[1]
+            # knn_lid_and_hubness_weighted_score = accuracy_scores.get(classifiers[3]._name)[1]
             #
             # knn_lid_weighted_scores.append(knn_lid_weighted_score)
             # knn_lid_and_hubness_weighted_scores.append(knn_lid_and_hubness_weighted_score)
@@ -172,5 +209,5 @@ if __name__ == '__main__':
         plt.ylabel('Score')
         plt.legend()
 
-        # plt.savefig(f'plots/{dataset}.png')
-        plt.show()
+        plt.savefig(f'plots/{dataset}.png')
+        # plt.show()
